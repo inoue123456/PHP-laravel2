a92262b3ed830a9676a872040e4ba278259e16d8
inoue123456 <yuta1511814@gmail.com>
2022-03-14 11:13:31 +0000

    laravel10task

---
+++ resources/views/admin/profile/create.blade.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta charset="utf-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+
+        <title>Laravel10task4 create</title>
+    </head>
+    <body>
+        <h1>practice create</h1>
+        <ul>
+            <li>a</li>
+        </ul>
+    </body>
+</html>
---
+++ resources/views/admin/profile/edit.blade.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta charset="utf-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+
+        <title>Laravel10task4 edit</title>
+    </head>
+    <body>
+        <h1>practice edit</h1>
+    </body>
+</html>
