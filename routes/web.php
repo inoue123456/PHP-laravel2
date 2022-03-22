7fbac57994f53f87bdf581948a9f7a52e04fdc5b
inoue123456 <yuta1511814@gmail.com>
2022-03-22 12:34:01 +0000

    laravel13task

--- routes/web.php
+++ routes/web.php
@@ -19,6 +19,9 @@ Route::group(['prefix' => 'admin'], function() {
     Route::get('news/create', 'Admin\NewsController@add')->middleware('auth');
     Route::get('profile/create', 'Admin\ProfileController@add')->middleware('auth');
     Route::get('profile/edit', 'Admin\ProfileController@edit')->middleware('auth');
+    Route::post('news/create', 'Admin\NewsController@create');
+    Route::post('profile/create', 'Admin\ProfileController@create');
+    Route::post('profile/edit', 'Admin\ProfileController@update');
 });
 
 
